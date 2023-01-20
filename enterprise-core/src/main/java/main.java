68f906048bb96a155b55351ab5213ea7d3dbc293
Z Jav <56839787+ZJav1310@users.noreply.github.com>
2023-01-20 17:50:02 +0000

    before cloud.

--- enterprise-core/src/main/java/main.java
+++ enterprise-core/src/main/java/main.java
@@ -12,21 +12,21 @@ import static org.reflections.Reflections.log;
 
 public class main {
     public static void main(String[] args) throws IOException {
-        XMLReader reader;
-        String PATH = null,
-                JsonString,
-                XmlString = "";
-
-        Film film = new Film(1, "Something", 1, "me", "stars", "reviewssasadasdadasds");
-
-        JsonString = "{\"id\":1,\"title\":\"Something\",\"year\":1,\"director\":\"me\",\"stars\":\"stars\",\"review\":\"reviewsss\"}\n";
-        XmlString = "<Film xmlns=\"\"><id>1</id><title>Something</title><year>1</year><director>me</director><stars>stars</stars><review>reviewsss</review></Film>\n";
-
-        Film filmA = XMLReader.getInstance().deserialiseObject(XmlString, Film.class);
-        Film filmB = JSONReader.getInstance().deserialiseObject(JsonString, Film.class);
-
-        System.out.println(filmA.toString());
-        System.out.println(JSONReader.getInstance().serialiseObject(film));
+//        XMLReader reader;
+//        String PATH = null,
+//                JsonString,
+//                XmlString = "";
+//
+//        Film film = new Film(1, "Something", 1, "me", "stars", "reviewssasadasdadasds");
+//
+//        JsonString = "{\"id\":1,\"title\":\"Something\",\"year\":1,\"director\":\"me\",\"stars\":\"stars\",\"review\":\"reviewsss\"}\n";
+//        XmlString = "<Film xmlns=\"\"><id>1</id><title>Something</title><year>1</year><director>me</director><stars>stars</stars><review>reviewsss</review></Film>\n";
+//
+//        Film filmA = XMLReader.getInstance().deserialiseObject(XmlString, Film.class);
+//        Film filmB = JSONReader.getInstance().deserialiseObject(JsonString, Film.class);
+//
+//        System.out.println(filmA.toString());
+//        System.out.println(JSONReader.getInstance().serialiseObject(film));
 
         try {
             DatabaseBuilder databaseBuilder = new DatabaseBuilder();
@@ -40,12 +40,12 @@ public class main {
             throw new RuntimeException(e);
         }
 
-        TextReader textReader = TextReader.getInstance();
-        System.out.println(textReader.serialiseObject(film));
-        String text = "#1|Something|1|me|stariis|reviewssasadasdasdsdfsfsfsdasds";
-        Film filmC = textReader.deserialiseObject(text, Film.class);
-
-        System.out.println(filmC.toString());
+//        TextReader textReader = TextReader.getInstance();
+//        System.out.println(textReader.serialiseObject(film));
+//        String text = "#1|Something|1|me|stariis|reviewssasadasdasdsdfsfsfsdasds";
+//        Film filmC = textReader.deserialiseObject(text, Film.class);
+//
+//        System.out.println(filmC.toString());
 
     }
 }
